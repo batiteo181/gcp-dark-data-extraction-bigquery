dd0ca13a51f46c941035c8de9f6f5aa1f323701b
ndtuan181 <nguyendinhtuan181@gmail.com>
2026-05-26 15:24:02 +0700

    Create 03_ultimate_test_join.sql

---
+++ sql/03_ultimate_test_join.sql
@@ -0,0 +1,17 @@
+-- TRUY VẤN ĐÍCH (ULTIMATE TEST): 
+-- Thực thi phép nối đa bảng (Multi-table JOIN) trên dữ liệu đã được làm sạch để trích xuất thông tin dị ứng
+SELECT 
+  p.product_name, 
+  i.ingredient_name, 
+  a.allergen_name, 
+  p.category, 
+  p.stability
+FROM `code-vipassana-497502.froyo_data.consistsof` c
+INNER JOIN `code-vipassana-497502.froyo_data.product` p 
+  ON c.product_id = p.product_id
+INNER JOIN `code-vipassana-497502.froyo_data.ingredient` i 
+  ON c.ingredient_id = i.ingredient_id
+LEFT OUTER JOIN `code-vipassana-497502.froyo_data.containsallergen` a 
+  ON i.ingredient_id = a.ingredient_id
+WHERE UPPER(p.product_name) LIKE '%MIDNIGHT%SWIRL%' 
+  AND a.allergen_name IS NOT NULL;
