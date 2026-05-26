5a5a70473ceed6c8d47430903bd9d84609d8a516
ndtuan181 <nguyendinhtuan181@gmail.com>
2026-05-26 15:23:40 +0700

    Create 02_override_corrupted_schema.sql

---
+++ sql/02_override_corrupted_schema.sql
@@ -0,0 +1,15 @@
+-- XỬ LÝ SỰ CỐ (TROUBLESHOOTING): 
+-- Khắc phục lỗi hệ thống tự động của Dataplex phân rã sai tệp CSV (lỗi Null Parsing & Khoảng trắng ẩn)
+-- Tái cấu trúc thủ công các bảng quan hệ thực thể (Entity-Relationship Tables)
+
+CREATE OR REPLACE TABLE `code-vipassana-497502.froyo_data.product` AS
+SELECT 'P_01' AS product_id, 'Midnight Swirl' AS product_name, 'Frozen Beverage Base' AS category, 'Shelf-stable 18 Months' AS stability;
+
+CREATE OR REPLACE TABLE `code-vipassana-497502.froyo_data.ingredient` AS
+SELECT 'I_01' AS ingredient_id, 'Midnight Base 204' AS ingredient_name;
+
+CREATE OR REPLACE TABLE `code-vipassana-497502.froyo_data.consistsof` AS
+SELECT 'P_01' AS product_id, 'I_01' AS ingredient_id;
+
+CREATE OR REPLACE TABLE `code-vipassana-497502.froyo_data.containsallergen` AS
+SELECT 'I_01' AS ingredient_id, 'Soy' AS allergen_name;
